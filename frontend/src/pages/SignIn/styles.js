e5f0f73608df37e112d0fd74e90092399642daf7
Sérgio Camargo <sergiocamargo81@gmail.com>
2019-12-05 17:05:01 -0300

    gympoint - frontend loading authentication

--- frontend/src/pages/SignIn/styles.js
+++ frontend/src/pages/SignIn/styles.js
@@ -5,7 +5,7 @@ export const Container = styled.div`
   width: 350px;
   background: #fff;
   border-radius: 4px;
-  padding: 30px 20px;
+  padding: 20px 20px;
 
   display: flex;
   flex-direction: column;
@@ -18,19 +18,19 @@ export const Container = styled.div`
   form {
     display: flex;
     flex-direction: column;
-    margin-top: 30px;
+    margin-top: 20px;
 
     label {
       font-weight: bold;
       display: flex;
       flex-direction: column;
       color: #444444;
-      margin: 10px 0 20px;
+      margin: 15px 0 0;
 
       input {
         height: 44px;
         padding: 0 15px;
-        margin: 5px 0 10px;
+        margin: 5px 0 0;
         border: 1px solid #dddddd;
         border-radius: 4px;
 
@@ -42,12 +42,12 @@ export const Container = styled.div`
 
     span {
       color: red;
-      margin: 0 0 10px;
+      margin: 5px 0 5px;
       font-weight: bold;
     }
 
     button {
-      margin: 5px 0 0;
+      margin: 20px 0 0;
       height: 44px;
       background: #ee4d64;
       font-weight: bold;

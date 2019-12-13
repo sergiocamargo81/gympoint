05519ac3278f5bdb5158b89e157f21c6b6b481e0
Sérgio Camargo <sergiocamargo81@gmail.com>
2019-12-13 18:51:14 -0300

    gympoint - frontend membership layout

--- frontend/src/pages/Memberships/index.js
+++ frontend/src/pages/Memberships/index.js
@@ -1,7 +1,78 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdAdd, MdCheck } from 'react-icons/md';
+
+import { Container, Panel, Content, MembershipsTable } from './styles';
 
 export default function Memberships() {
-  return <div />;
+  return (
+    <Container>
+      <Panel>
+        <span>Gerenciando matrículas</span>
+        <button type="button">
+          <MdAdd size={20} />
+          <span>Cadastrar</span>
+        </button>
+      </Panel>
+      <Content>
+        <MembershipsTable>
+          <thead>
+            <tr>
+              <th>ALUNO</th>
+              <th>PLANO</th>
+              <th> </th>
+              <th>INÍCIO</th>
+              <th> </th>
+              <th>TÉRMINO</th>
+              <th> </th>
+              <th>ATIVA</th>
+              <th> </th>
+              <th> </th>
+              <th> </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Sérgio Camargo</td>
+              <td>Start</td>
+              <td> </td>
+              <td>30 de Abril de 2019</td>
+              <td> </td>
+              <td>30 de Maio de 2019</td>
+              <td> </td>
+              <td>
+                <MdCheck size={20} className="active" />
+              </td>
+              <td> </td>
+              <td>
+                <button type="button">editar</button>
+              </td>
+              <td>
+                <button type="button">apagar</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Antonio de Camargo</td>
+              <td>Diamond</td>
+              <td> </td>
+              <td>14 de Fevereiro de 2019</td>
+              <td> </td>
+              <td>14 de Fevereiro de 2020</td>
+              <td> </td>
+              <td>
+                <MdCheck size={20} />
+              </td>
+              <td> </td>
+              <td>
+                <button type="button">editar</button>
+              </td>
+              <td>
+                <button type="button">apagar</button>
+              </td>
+            </tr>
+          </tbody>
+        </MembershipsTable>
+      </Content>
+    </Container>
+  );
 }
---
+++ frontend/src/pages/Memberships/styles.js
@@ -0,0 +1,227 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  align-self: center;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 1380px;
+`;
+
+export const Panel = styled.div`
+  width: inherit;
+  display: flex;
+  justify-content: space-between;
+  margin: 30px 0 20px;
+
+  span {
+    flex-grow: 1;
+    font-size: 24px;
+    font-weight: bold;
+  }
+
+  button {
+    width: 142px;
+    height: 37px;
+    text-transform: uppercase;
+    background: #ee4d64;
+    color: #fff;
+    border: 0;
+    border-radius: 4px;
+    transition: background 0.2s;
+
+    &:hover {
+      background: ${darken(0.1, '#ee4d64')};
+    }
+
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+
+    span {
+      font-weight: bold;
+      font-size: 14px;
+      border: 0;
+      margin: 0 15px 0 0;
+      flex-grow: 0;
+    }
+
+    svg {
+      flex-grow: 0;
+      margin: 0 0 0 15px;
+    }
+  }
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  background: #fff;
+  padding: 30px 30px 15px 30px;
+  border-radius: 4px;
+`;
+
+export const MembershipsTable = styled.table`
+  border-spacing: 0;
+
+  > thead {
+    > tr {
+      > th {
+        text-align: left;
+        font-size: 16px;
+        font-weight: bold;
+        white-space: nowrap;
+        padding-bottom: 5px;
+      }
+
+      /* Aluno */
+      > th:nth-child(1) {
+        width: 330px;
+      }
+
+      /* Plano */
+      > th:nth-child(2) {
+        width: 130px;
+        text-align: center;
+      }
+
+      /* Blank(1) */
+      > th:nth-child(3) {
+        width: 70px;
+      }
+
+      /* Início */
+      > th:nth-child(4) {
+        width: 210px;
+        text-align: center;
+      }
+
+      /* Blank(2) */
+      > th:nth-child(5) {
+        width: 55px;
+      }
+
+      /* Término */
+      > th:nth-child(6) {
+        width: 210px;
+        text-align: center;
+      }
+
+      /* Blank(3) */
+      > th:nth-child(7) {
+        width: 70px;
+      }
+
+      /* Ativa */
+      > th:nth-child(8) {
+        width: 60px;
+        text-align: center;
+      }
+
+      /* Blank(4) */
+      > th:nth-child(9) {
+        width: 45px;
+      }
+
+      /* Editar */
+      > th:nth-child(10) {
+        width: 70px;
+      }
+
+      /* Apagar */
+      > th:nth-child(11) {
+        width: 70px;
+      }
+    }
+  }
+
+  > tbody {
+    > tr {
+      width: 1150;
+      color: #666;
+      font-size: 16px;
+
+      > td {
+        padding: 15px 0;
+        border-bottom: 1px solid #eee;
+      }
+
+      /* Aluno */
+      > td:nth-child(1) {
+        text-align: left;
+      }
+
+      /* Plano */
+      > td:nth-child(2) {
+        text-align: center;
+      }
+
+      /* Início */
+      > td:nth-child(4) {
+        text-align: center;
+      }
+
+      /* Término */
+      > td:nth-child(6) {
+        text-align: center;
+      }
+
+      /* Ativa */
+      > td:nth-child(8) {
+        text-align: center;
+
+        > svg {
+          color: #fff;
+          border-radius: 50%;
+          background: #ddd;
+        }
+
+        > svg.active {
+          background: #42cb59;
+        }
+      }
+
+      /* Editar */
+      > td:nth-child(10) {
+        text-align: right;
+
+        > button {
+          font-size: 15px;
+          color: #4d85ee;
+          text-align: right;
+          background: transparent;
+          border: 0;
+          transition: font-weight 0.2s;
+
+          &:hover {
+            font-weight: bold;
+          }
+        }
+      }
+
+      /* Apagar */
+      > td:nth-child(11) {
+        text-align: right;
+
+        > button {
+          font-size: 15px;
+          color: #de3b3b;
+          text-align: right;
+          background: transparent;
+          border: 0;
+          transition: font-weight 0.2s;
+
+          &:hover {
+            font-weight: bold;
+          }
+        }
+      }
+    }
+
+    > tr:last-child {
+      > td {
+        border-bottom: 0;
+      }
+    }
+  }
+`;

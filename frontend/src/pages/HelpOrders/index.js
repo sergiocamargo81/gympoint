b34185d338af6188e299bb9bbc89744a3c970fb7
Sérgio Camargo <sergiocamargo81@gmail.com>
2019-12-13 19:11:41 -0300

    gympoint - frontend helporders layout

--- frontend/src/pages/HelpOrders/index.js
+++ frontend/src/pages/HelpOrders/index.js
@@ -1,7 +1,43 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Panel, Content, HelpOrdersTable } from './styles';
 
 export default function HelpOrders() {
-  return <div />;
+  return (
+    <Container>
+      <Panel>
+        <span>Pedidos de auxílio</span>
+      </Panel>
+      <Content>
+        <HelpOrdersTable>
+          <thead>
+            <tr>
+              <th>ALUNO</th>
+              <th> </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Sérgio Camargo</td>
+              <td>
+                <button type="button">responder</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Sérgio Camargo</td>
+              <td>
+                <button type="button">responder</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Antonio de Camargo</td>
+              <td>
+                <button type="button">responder</button>
+              </td>
+            </tr>
+          </tbody>
+        </HelpOrdersTable>
+      </Content>
+    </Container>
+  );
 }
---
+++ frontend/src/pages/HelpOrders/styles.js
@@ -0,0 +1,96 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  align-self: center;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 700px;
+`;
+
+export const Panel = styled.div`
+  width: inherit;
+  display: flex;
+  justify-content: space-between;
+  margin: 30px 0 20px;
+
+  span {
+    flex-grow: 1;
+    font-size: 24px;
+    font-weight: bold;
+  }
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  background: #fff;
+  padding: 30px 30px 15px 30px;
+  border-radius: 4px;
+`;
+
+export const HelpOrdersTable = styled.table`
+  border-spacing: 0;
+
+  > thead {
+    > tr {
+      > th {
+        text-align: left;
+        font-size: 16px;
+        font-weight: bold;
+        white-space: nowrap;
+        padding-bottom: 5px;
+      }
+
+      /* Aluno */
+      > th:nth-child(1) {
+        width: 330px;
+      }
+
+      /* Responder */
+      > th:nth-child(2) {
+        width: 330px;
+      }
+    }
+  }
+
+  > tbody {
+    > tr {
+      color: #666;
+      font-size: 16px;
+
+      > td {
+        padding: 15px 0;
+        border-bottom: 1px solid #eee;
+      }
+
+      /* Aluno */
+      > td:nth-child(1) {
+        text-align: left;
+      }
+
+      /* Responder */
+      > td:nth-child(2) {
+        text-align: right;
+
+        > button {
+          font-size: 15px;
+          color: #4d85ee;
+          text-align: right;
+          background: transparent;
+          border: 0;
+          transition: font-weight 0.2s;
+
+          &:hover {
+            font-weight: bold;
+          }
+        }
+      }
+    }
+
+    > tr:last-child {
+      > td {
+        border-bottom: 0;
+      }
+    }
+  }
+`;

720a3b71bf37c8c842cf255e84efb7ae8709ab7d
Sérgio Camargo <sergiocamargo81@gmail.com>
2019-12-16 16:32:36 -0300

    gympoint - frontend membership

--- frontend/src/pages/Membership/index.js
+++ frontend/src/pages/Membership/index.js
@@ -1,7 +1,199 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
-import { Container } from './styles';
+import { toast } from 'react-toastify';
 
-export default function Membership() {
-  return <Container></Container>;
+import * as Yup from 'yup';
+
+import { Link } from 'react-router-dom';
+
+import { MdChevronLeft, MdCheck } from 'react-icons/md';
+
+import PropTypes from 'prop-types';
+
+import AsyncSelect from 'react-select/async';
+
+import DatePicker, { registerLocale } from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import pt from 'date-fns/locale/pt';
+
+import api from '~/services/api';
+
+import { Container, Panel, MembershipData } from './styles';
+
+registerLocale('pt', pt);
+
+const reactSelectStudentOptions = inputValue => {
+  if (!inputValue) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve([]);
+      }, 1000);
+    });
+  }
+
+  return api
+    .get(`students/?nameFilter=${inputValue}&page=1&pageSize=10`)
+    .then(response =>
+      response.data.students.map(s => ({
+        value: s.id,
+        label: s.name,
+      }))
+    )
+    .catch(error => toast.error(error.toString()));
+};
+
+const reactSelectPlanOptions = inputValue => {
+  if (!inputValue) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve([]);
+      }, 1000);
+    });
+  }
+
+  return api
+    .get(`plans/?nameFilter=${inputValue}&page=1&pageSize=10`)
+    .then(response =>
+      response.data.plans.map(s => ({
+        value: s.id,
+        label: s.name,
+      }))
+    )
+    .catch(error => toast.error(error.toString()));
+};
+
+export default function Membership({ history }) {
+  const { membership } = history.location.state;
+
+  async function handleSubmit({ student_id, plan_id, start_date }) {
+    const id = Number(membership.id);
+
+    const _then = () => history.push('memberships');
+
+    const _catch = e => {
+      let message;
+
+      if (e.response) {
+        message = e.response.data.error;
+      } else if (e.request) {
+        message = e.request.toString();
+      } else {
+        message = e.message;
+      }
+
+      toast.error(message);
+    };
+
+    await (id
+      ? api.put('memberships/', {
+          student_id,
+          plan_id,
+          start_date,
+        })
+      : api.post('memberships/', { student_id, plan_id, start_date })
+    )
+      .then(_then)
+      .catch(_catch);
+  }
+
+  /*
+  const total = useMemo(
+    () =>
+      new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(membership.plan.duration * membership.plan.price),
+    [membership.plan.duration, membership.plan.price]
+  );
+  */
+
+  const title = membership.id ? 'Edição de matrícula' : 'Cadastro de matrícula';
+
+  const handleInputChange = newValue => {
+    const inputValue = newValue.replace(/\W/g, '');
+
+    return inputValue;
+  };
+
+  /*
+  const filterStudents = inputValue => {
+    async function loadStudents() {
+      const response = await api.get(
+        `students/?nameFilter=${inputValue}&page=1&pageSize=10`
+      );
+
+      return response.data.students;
+    }
+
+    return loadStudents();
+  };
+
+  const loadOptions = inputValue =>
+    new Promise(resolve => {
+      setTimeout(() => {
+        resolve(filterStudents(inputValue));
+      }, 100);
+    });
+  */
+
+  return (
+    <Container>
+      <Panel>
+        <span>{title}</span>
+        <Link to="memberships">
+          <button type="button">
+            <MdChevronLeft size={20} />
+            <span>voltar</span>
+          </button>
+        </Link>
+        <button type="submit" className="save">
+          <MdCheck size={20} />
+          <span>salvar</span>
+        </button>
+      </Panel>
+      <MembershipData>
+        <label>
+          ALUNO
+          <AsyncSelect
+            cacheOptions
+            loadOptions={reactSelectStudentOptions}
+            defaultOptions
+            onInputChange={handleInputChange}
+            loadingMessage={() => 'Buscando alunos...'}
+            placeholder="Buscar aluno"
+          />
+        </label>
+        <div>
+          <label>
+            PLANO
+            <AsyncSelect
+              cacheOptions
+              loadOptions={reactSelectPlanOptions}
+              defaultOptions
+              onInputChange={handleInputChange}
+              loadingMessage={() => 'Buscando planos...'}
+              placeholder="Selecione"
+            />
+          </label>
+          <label>
+            DATA DE INÍCIO
+            <DatePicker
+              locale="pt"
+              placeholderText="Escolha a data"
+              // selected={this.state.startDate}
+              // onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            DATA DE TÉRMINO
+            <span>{0}</span>
+          </label>
+          <label>
+            VALOR FINAL
+            <span>{0}</span>
+          </label>
+        </div>
+      </MembershipData>
+    </Container>
+  );
 }
--- frontend/src/pages/Membership/styles.js
+++ frontend/src/pages/Membership/styles.js
@@ -1,3 +1,171 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 900px;
+  align-self: center;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  > form {
+    width: 100%;
+  }
+`;
+
+export const Panel = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin: 30px 0 20px;
+
+  span {
+    flex-grow: 1;
+    font-size: 24px;
+    font-weight: bold;
+  }
+
+  button {
+    width: 110px;
+    height: 37px;
+    text-transform: uppercase;
+    background: #ccc;
+    color: #fff;
+    border: 0;
+    border-radius: 4px;
+    transition: background 0.2s;
+
+    &:hover {
+      background: ${darken(0.1, '#ccc')};
+    }
+
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+
+    span {
+      font-weight: bold;
+      font-size: 14px;
+      border: 0;
+      margin: 0 15px 0 0;
+      flex-grow: 0;
+    }
+
+    svg {
+      flex-grow: 0;
+      margin: 0 0 0 15px;
+    }
+  }
+
+  > button.save {
+    background: #ee4d64;
+    margin-left: 20px;
+    transition: background 0.2s;
+
+    &:hover {
+      background: ${darken(0.1, '#ee4d64')};
+    }
+  }
+`;
+
+export const MembershipData = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  background: #fff;
+  border-radius: 4px;
+  padding: 30px 30px 10px 30px;
+
+  label {
+    font-weight: bold;
+    font-size: 14px;
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 14px;
+  }
+
+  /* Aluno */
+  > label:nth-child(1) {
+    > div {
+      margin: 6px 0 6px 0;
+
+      > div {
+        height: 45px;
+
+        > div:nth-child(1) {
+          /* placeholder */
+          > div {
+            font-size: 16px;
+            font-weight: normal;
+          }
+        }
+      }
+    }
+  }
+
+  > div {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    > label {
+      width: 195px;
+
+      > div > div > input:first-child {
+        width: 195px;
+        font-size: 16px;
+        padding: 12px;
+        border-radius: 4px;
+        border: 1px solid #ddd;
+        margin: 6px 0 6px 0;
+        max-height: 45px;
+        color: #444;
+
+        &::placeholder {
+          color: #999;
+        }
+      }
+
+      > span:first-child {
+        width: 195px;
+        font-size: 16px;
+        flex-grow: 1;
+        padding: 12px;
+        border-radius: 4px;
+        border: 1px solid #ddd;
+        margin: 6px 0 6px 0;
+        max-height: 45px;
+        color: #444;
+        font-weight: normal;
+        display: block;
+        background: #f5f5f5;
+      }
+    }
+
+    /* Plano */
+    > label:nth-child(1) {
+      > div {
+        width: 195px;
+        margin: 6px 0 6px 0;
+
+        > div {
+          height: 45px;
+
+          > div:nth-child(1) {
+            /* placeholder */
+            > div {
+              font-size: 16px;
+              font-weight: normal;
+            }
+          }
+        }
+      }
+    }
+
+    /* Data de início */
+    > label:nth-child(2) {
+      > div {
+      }
+    }
+  }
+`;

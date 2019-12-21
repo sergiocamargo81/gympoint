f924959e6ff6481810475a439f146c5d1b9a4c54
Sérgio Camargo <sergiocamargo81@gmail.com>
2019-12-21 14:44:28 -0300

    mobile - help orders

--- mobile/src/pages/HelpOrders/index.js
+++ mobile/src/pages/HelpOrders/index.js
@@ -1,19 +1,125 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+
+import { Alert } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import { useSelector } from 'react-redux';
+
+import PropTypes from 'prop-types';
+
+import { formatDistanceToNow, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt-BR';
+import api from '~/services/api';
+
+import {
+  Container,
+  Button,
+  List,
+  HelpOrder,
+  HelpOrderHeader,
+  HelpOrderCheck,
+  HelpOrderStatus,
+  HelpOrderElapsed,
+  HelpOrderBody,
+} from './styles';
+
+import Header from '~/components/Header';
+
+function ItemStatus({ item }) {
+  return (
+    <HelpOrderStatus answered={item.answered}>
+      {item.answered ? 'Respondido' : 'Sem resposta'}
+    </HelpOrderStatus>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <HelpOrder>
+      <HelpOrderHeader>
+        <HelpOrderCheck answered={item.answered} />
+        <ItemStatus item={item} />
+        <HelpOrderElapsed>{item.elapsed}</HelpOrderElapsed>
+      </HelpOrderHeader>
+      <HelpOrderBody numberOfLines={3}>{item.question}</HelpOrderBody>
+    </HelpOrder>
+  );
+}
 
 export default function HelpOrders() {
-  return <View />;
+  const id = useSelector(state => state.auth.id);
+
+  const [helpOrders, setHelpOrders] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+
+  const [newHelpOrder, setNewHelpOrder] = useState({});
+
+  useEffect(() => {
+    async function loadHelpOrders() {
+      const response = await api.get(`students/${id}/help-orders`);
+
+      const helpOrdersFormatted = response.data.map(h => {
+        h.answered = h.answer !== null;
+        h.elapsed = formatDistanceToNow(parseISO(h.createdAt), {
+          addSuffix: true,
+          locale: pt,
+        });
+
+        return h;
+      });
+
+      setHelpOrders(helpOrdersFormatted);
+    }
+
+    loadHelpOrders();
+  }, [id, newHelpOrder]);
+
+  async function handleNewHelpOrder() {
+    /*
+    setLoading(true);
+
+    try {
+      const response = await api.post(`/students/${id}/checkins`);
+
+      setNewHelpOrder(response.data);
+    } catch (e) {
+      let message;
+
+      if (e.response) {
+        message = e.response.data.error;
+      } else if (e.request) {
+        message = e.request.toString();
+      } else {
+        message = e.message;
+      }
+
+      Alert.alert('Falha ao realizar check-in', message);
+    }
+
+    setLoading(false);
+    */
+  }
+
+  return (
+    <Container>
+      <Header />
+      <Button loading={loading} onPress={handleNewHelpOrder}>
+        Novo pedido de auxílio
+      </Button>
+      <List
+        data={helpOrders}
+        keyExtractor={item => String(item.id)}
+        renderItem={({ item }) => <Item item={item} />}
+      />
+    </Container>
+  );
 }
 
 HelpOrders.navigationOptions = {
   tabBarLabel: 'Pedir ajuda',
   tabBarIcon: ({ tintColor }) => (
-    <View>
-      <Icon name="live-help" size={20} color={tintColor} />
-    </View>
+    <Icon name="live-help" size={20} color={tintColor} />
   ),
 };
---
+++ mobile/src/pages/HelpOrders/styles.js
@@ -0,0 +1,65 @@
+import styled from 'styled-components/native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import ButtonComponent from '~/components/Button';
+
+export const Container = styled.View`
+  flex: 1;
+  background: #f5f5f5;
+  border-bottom-color: #ddd;
+  border-bottom-width: 1px;
+`;
+
+export const Button = styled(ButtonComponent)`
+  margin: 25px 20px;
+`;
+
+export const List = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})``;
+
+export const HelpOrder = styled.View`
+  align-items: flex-start;
+  background: #fff;
+  border: solid 1px #ddd;
+  margin: 0 20px 15px;
+  border-radius: 4px;
+  height: 150px;
+  padding: 20px;
+`;
+
+export const HelpOrderHeader = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  height: 16px;
+`;
+
+export const HelpOrderCheck = styled(Icon).attrs(props => ({
+  name: 'check-circle',
+  color: props.answered ? '#42cb59' : '#999',
+  size: 16,
+}))`
+  margin-right: 5px;
+`;
+
+export const HelpOrderStatus = styled.Text`
+  flex-grow: 1;
+  color: ${props => (props.answered ? '#42cb59' : '#999')};
+`;
+
+export const HelpOrderElapsed = styled.Text`
+  font-size: 14px;
+  color: #666;
+  line-height: 26px;
+  text-align: right;
+`;
+
+export const HelpOrderBody = styled.Text`
+  margin-top: 20px;
+  text-align: left;
+  font-size: 14px;
+  color: #666;
+  line-height: 26px;
+`;

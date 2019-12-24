da8728572c2893ef876826025457195980205ad1
Sérgio Camargo <sergiocamargo81@gmail.com>
2019-12-23 23:10:15 -0300

    backend - session student changed

--- backend/src/app/controllers/StudentSessionController.js
+++ backend/src/app/controllers/StudentSessionController.js
@@ -1,5 +1,20 @@
+import Membership from '../models/Membership';
+
 class StudentSessionController {
   async index(req, res) {
+    const student_id = req.student.id;
+    const active = true;
+
+    const result = await Membership.findAll({
+      where: { student_id },
+    });
+
+    if (result.filter(m => m.active === true).length === 0) {
+      return res
+        .status(400)
+        .json({ error: 'Student does not have a active membership' });
+    }
+
     return res.json(req.student);
   }
 }
